920659167767766371afbab30dc6dc635ee9473e
ari <tj.fake4@gmail.com>
2014-05-26 20:07:30 +0900

    ver1.2

--- envelope.py
+++ envelope.py
@@ -2,31 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def f(x,t):
-	return t*x-t**2
+	return t*x-t**2 #関数を設定
 
 def subplots():
-    "Custom subplots with axes throught the origin"
     fig, ax = plt.subplots()
 
-    # Set the axes through the origin
     for spine in ['left', 'bottom']:
-        ax.spines[spine].set_position('zero')
+        ax.spines[spine].set_position('zero') #左と下の軸は中央に位置させる 
+        
     for spine in ['right', 'top']:
-        ax.spines[spine].set_color('none')
+        ax.spines[spine].set_color('none') #右と上の軸は色を消す
     
-    ax.grid()
     return (fig, ax)
 
-fig, ax = subplots()  # Call the local version, not plt.subplots()
-x = np.linspace(-10, 10, 200)
+fig, ax = subplots() 
 
-for s in range(10):
- t = 5-s
+x = np.linspace(-10, 10, 200) #xの範囲を決める
+
+for s in range(-20,20): #tでループさせる値を決める
+ t = s*0.3+0.1
  y = f(x,t) 
- ax.plot(x,y,'b-',linewidth=1)
- ax.legend() 
+ ax.plot(x,y,'k-',linewidth=1)
+ 
+plt.tick_params(labelbottom='off') #x軸の目盛りを消す
+plt.tick_params(labelleft='off') #y軸の目盛りを消す
 
-plt.tick_params(labelbottom='off')
-plt.tick_params(labelleft='off')
-plt.ylim(-20,30)
+plt.ylim(-20,30) #yの表示範囲を決める
+
 plt.show()
